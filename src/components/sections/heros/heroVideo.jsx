45fff113e919c8720290a4ff72c124e36dc7fda0
yoginandal <yogidesign1996@gmail.com>
2024-10-24 12:29:43 +0530

    banner-updated

--- src/components/sections/heros/heroVideo.jsx
+++ src/components/sections/heros/heroVideo.jsx
@@ -1,20 +1,38 @@
 const HeroVideo = () => {
   return (
-    <section className="relative w-full" style={{ paddingTop: "56.25%" }}>
-      {" "}
-      {/* 16:9 aspect ratio */}
-      <video
-        className="absolute top-0 left-0 w-full h-full object-cover"
-        autoPlay
-        muted
-        loop
-      >
-        <source
-          src="https://www.bfis.in/wp-content/themes/bfis-theme/js/video.mp4"
-          type="video/mp4"
-        />
-        Your browser does not support the video tag.
-      </video>
+    <section className="relative w-full">
+      {/* Video for mobile */}
+      <div className="block md:hidden h-screen">
+        <video
+          className="absolute top-0 left-0 w-full h-full object-cover"
+          autoPlay
+          muted
+          loop
+          playsInline
+        >
+          <source
+            src="https://www.bfis.in/wp-content/themes/bfis-theme/js/video2.mp4"
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+
+      {/* Video for desktop */}
+      <div className="hidden md:block h-screen">
+        <video
+          className="absolute top-0 left-0 w-full h-full object-cover"
+          autoPlay
+          muted
+          loop
+        >
+          <source
+            src="https://www.bfis.in/wp-content/themes/bfis-theme/js/video.mp4"
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
+      </div>
     </section>
   );
 };
